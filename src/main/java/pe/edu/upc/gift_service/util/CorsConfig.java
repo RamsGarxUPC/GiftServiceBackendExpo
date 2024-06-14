b7ad47bdb3f0a815c865781205bc320b26ee0195
Ramiro García Cárdenas <u202120230@upc.edu.pe>
2024-06-14 18:01:33 -0500

    Commit para Backend Expo con PDetails corregido con link frontend sin slash

--- src/main/java/pe/edu/upc/gift_service/util/CorsConfig.java
+++ src/main/java/pe/edu/upc/gift_service/util/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://prueba-despliegue-frontend-expo.netlify.app/")  // Ajusta el origen permitido
+                        .allowedOrigins("http://localhost:4200", "https://prueba-despliegue-frontend-expo.netlify.app")  // Ajusta el origen permitido
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") //eliminas los que no uses
                         .allowedHeaders("*")
                         .allowCredentials(true);
