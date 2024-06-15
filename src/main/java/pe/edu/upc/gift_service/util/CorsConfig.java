47533757dd903a4da6bd07c528bd2e09b615fb7f
Ramiro García Cárdenas <u202120230@upc.edu.pe>
2024-06-14 20:47:12 -0500

    Commit para expo front 2

--- src/main/java/pe/edu/upc/gift_service/util/CorsConfig.java
+++ src/main/java/pe/edu/upc/gift_service/util/CorsConfig.java
@@ -2,22 +2,19 @@ package pe.edu.upc.gift_service.util;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://dashing-sfogliatella-a0e9f9.netlify.app")  // Ajusta el origen permitido
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") //eliminas los que no uses
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/")
+                .allowedOrigins("http://localhost:4200", "https://dashing-sfogliatella-a0e9f9.netlify.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
